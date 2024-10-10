bc6ec9ee4048680ed8f6e74a6f93a63147daf286
Nekika <hugo.jahnke@gmail.com>
2024-10-10 22:36:23 +0200

    feat: send events to TCP server

--- lua/tulipe/init.lua
+++ lua/tulipe/init.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 function M.setup(_opts)
-  vim.cmd('echo "Hello, Tulipe!"')
+  local socket = require("socket").connect("localhost", 9898)
+
+  for _, event in ipairs(require("tulipe/events").Events) do
+    vim.api.nvim_create_autocmd(event, {
+      callback = function() socket:send(event .. '\n') end
+    })
+  end
 end
 
 return M
