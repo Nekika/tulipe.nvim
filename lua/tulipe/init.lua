7ce21dd443edf346733b89e57538c63a80d5c8d1
Nekika <hugo.jahnke@gmail.com>
2024-10-13 15:47:55 +0200

    feat/refactor: start function + connection error handling

--- lua/tulipe/init.lua
+++ lua/tulipe/init.lua
@@ -1,11 +1,29 @@
 local M = {}
 
-function M.setup(_opts)
-  local socket = require("socket").connect("localhost", 9898)
-
+local function start(client)
   for _, event in ipairs(require("tulipe/events").Events) do
     vim.api.nvim_create_autocmd(event, {
-      callback = function() socket:send(event .. '\n') end
+      callback = function() client:send(event .. '\n') end
+    })
+  end
+end
+
+function M.setup(opts)
+  local host = opts.host or "localhost"
+  local port = opts.port or 9898
+
+  local socket = require("socket")
+  local client = socket.connect(host, port)
+
+  if client == nil then
+    error("unable to connect to the server")
+  end
+
+  if vim.v.vim_did_enter then
+    start(client)
+  else
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function() start(client) end
     })
   end
 end
